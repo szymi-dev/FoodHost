7286bb166a1bab7ccf371e3bbc1612546f95115f
Sebastian <sebastian.mazur.p@gmail.com>
2023-08-29 14:54:54 +0200

    header/navbar

--- Client/src/themes.ts
+++ Client/src/themes.ts
@@ -2,6 +2,13 @@
 import { createTheme } from "@mui/material/styles";
 
 export const lightTheme = createTheme({
+  typography: {
+    fontFamily: ["Roboto", "Arial"].join(","),
+    h1: {
+      fontSize: "1.2rem",
+    },
+  },
+
   palette: {
     mode: "light",
     primary: {
@@ -19,10 +26,25 @@ export const lightTheme = createTheme({
     success: {
       main: "#66bb6a",
     },
+    text: {
+      primary: "#000",
+      secondary: "#666",
+    },
+    background: {
+      default: "#f7faf9",
+      paper: "#08c873",
+    },
   },
 });
 
 export const darkTheme = createTheme({
+  typography: {
+    fontFamily: ["Roboto", "Arial"].join(","),
+    h1: {
+      fontSize: "1.2rem",
+    },
+  },
+
   palette: {
     mode: "dark",
     primary: {
@@ -39,6 +61,14 @@ export const darkTheme = createTheme({
     },
     success: {
       main: "#08c873",
+    },
+    text: {
+      primary: "#fff", // Kolor tekstu na ciemnym tle
+      secondary: "#ccc", // Kolor tekstu pomocniczego na ciemnym tle
+    },
+    background: {
+      default: "#333", // Domyślne tło
+      paper: "#065a33",
     },
   },
 });
