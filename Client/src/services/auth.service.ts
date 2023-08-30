e6e2ffe31d9659ce52e808a61805c592bcfb2340
Sebastian <sebastian.mazur.p@gmail.com>
2023-08-30 19:52:53 +0200

    auth Service

---
+++ Client/src/services/auth.service.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:46566";
+interface LoginI {
+  email: string;
+  password: string;
+}
+const config = {
+  headers: { "Content-Type": "application/json" },
+};
+const login = ({ email, password }: LoginI) => {
+  return axios
+    .post(
+      API_URL + "/api/Account/login",
+      {
+        email,
+        password,
+      },
+      config
+    )
+    .then(response => {
+      if (response.data.token) {
+        localStorage.setItem("token", JSON.stringify(response.data.token));
+      }
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("token");
+};
+
+const authService = {
+  login,
+  logout,
+};
+
+export default authService;
