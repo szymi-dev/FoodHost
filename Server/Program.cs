01494d87366d3e20544d698d471789d56bb1ed63
Szymon Zieliński <sz.zielinski02@onet.pl>
2023-08-31 13:57:35 +0200

    Backend-CorsAdded

--- Server/Program.cs
+++ Server/Program.cs
@@ -42,7 +42,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
         ValidateAudience = false
     });
 
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy => policy.AllowAnyHeader()
+        .AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:5173"));
+});
 
 var app = builder.Build();
 
@@ -53,9 +57,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http:\\localhost:5173"));
+app.UseCors();
 
 app.UseAuthentication();
 
